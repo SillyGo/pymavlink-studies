8a24b8320aea06ce9c7a223751201e4db29713d9
SillyGo <sillygo@AshLey3.0>
2026-02-07 09:18:35 -0300

    first

---
+++ listen.py
@@ -0,0 +1,9 @@
+from pymavlink import mavutil
+
+connect = mavutil.mavlink_connection('udpin:localhost:14551')
+
+connect.wait_heartbeat()
+print("heartbeat registrada, drone conectado!")
+print(f"sistema {connect.target_system}, componente {connect.target_component}")
+
+
